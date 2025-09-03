896f6ff1cf9cc837e632ddc722762d11822ed1fb
NateDhaliwal <nathaniel.shaan@gmail.com>
2025-09-04 07:26:40 +0800

    Update init-nav-controls.js

--- javascripts/discourse/api-initializers/init-nav-controls.js
+++ javascripts/discourse/api-initializers/init-nav-controls.js
@@ -1,40 +1,35 @@
 import { service } from '@ember/service';
-import { withPluginApi } from "discourse/lib/plugin-api";
+import { apiInitializer } from "discourse/lib/plugin-api";
 
-export default {
-  name: "discourse-navigation-controls",
+export default apiInitializer((api) => {
+  // const site = api.container.lookup("service:site");
+  @service site;
+  
+  if (!this.site.mobileView) return;
+        
+  let scrollTop = window.scrollY;
+  const body = document.body;
+  const scrollMax = 0;
+  let lastScrollTop = 0;
+  const hiddenNavClass = "nav-controls-hidden";
 
-  initialize() {
-    withPluginApi("0.8.13", (api) => {
-      const site = container.lookup("service:site");
-      
-      if (!site.mobileView) return;
-            
-      let scrollTop = window.scrollY;
-      const body = document.body;
-      const scrollMax = 0;
-      let lastScrollTop = 0;
-      const hiddenNavClass = "nav-controls-hidden";
+  const add_class_on_scroll = () => body.classList.add(hiddenNavClass);
+  const remove_class_on_scroll = () => body.classList.remove(hiddenNavClass);
 
-      const add_class_on_scroll = () => body.classList.add(hiddenNavClass);
-      const remove_class_on_scroll = () => body.classList.remove(hiddenNavClass);
-
-      window.addEventListener('scroll', function() { 
-        scrollTop = window.scrollY;
-        if (
-          lastScrollTop < scrollTop &&
-          scrollTop > scrollMax &&
-          !body.classList.contains(hiddenNavClass)
-        ) { 
-          add_class_on_scroll();
-        } else if (
-          lastScrollTop > scrollTop &&
-          body.classList.contains(hiddenNavClass)
-        ) { 
-          remove_class_on_scroll();
-        }
-        lastScrollTop = scrollTop;
-      });
-    });
-  },
-};
+  window.addEventListener('scroll', function() { 
+    scrollTop = window.scrollY;
+    if (
+      lastScrollTop < scrollTop &&
+      scrollTop > scrollMax &&
+      !body.classList.contains(hiddenNavClass)
+    ) { 
+      add_class_on_scroll();
+    } else if (
+      lastScrollTop > scrollTop &&
+      body.classList.contains(hiddenNavClass)
+    ) { 
+      remove_class_on_scroll();
+    }
+    lastScrollTop = scrollTop;
+  });
+});

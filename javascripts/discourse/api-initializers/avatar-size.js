1f266336dae0581bb59fd44d068b2bc150addd6d
NateDhaliwal <nathaniel.shaan@gmail.com>
2025-09-04 07:34:26 +0800

    Update avatar-size.js

--- javascripts/discourse/api-initializers/avatar-size.js
+++ javascripts/discourse/api-initializers/avatar-size.js
@@ -1,31 +1,36 @@
 import { service } from '@ember/service';
+import { action } from '@ember/object';
+import Component from '@glimmer/component';
 import { withSilencedDeprecations } from "discourse/lib/deprecated";
 import { apiInitializer } from "discourse/lib/plugin-api";
 
-function avatarSize(api) {
-  // Change avatar size on desktop
-  api.registerValueTransformer(
-    "post-avatar-size",
-    () => 60
-  );
-  
-  // wrap the old widget code silencing the deprecation warnings
-  withSilencedDeprecations("discourse.post-stream-widget-overrides", () =>
-    oldAvatarSize(api)
-  );
-}
-
-// old widget code
-function oldAvatarSize(api) {
-  // Change avatar size on desktop
-  api.changeWidgetSetting("post-avatar", "size", 60);
-}
-
 export default apiInitializer((api) => {
   // const site = api.container.lookup("service:site");
-  @service site;
-  
-  if (!this.site.mobileView) {
-    avatarSize(api);
+  class AvatarSize extends Component {
+    @service site;
+
+    @action
+    avatarSize() {
+      // Change avatar size on desktop
+      api.registerValueTransformer(
+        "post-avatar-size",
+        () => 60
+      );
+      
+      // wrap the old widget code silencing the deprecation warnings
+      withSilencedDeprecations("discourse.post-stream-widget-overrides", () =>
+        oldAvatarSize(api)
+      );
+    }
+
+    // old widget code
+    get oldAvatarSize(/* api */) {
+      // Change avatar size on desktop
+      api.changeWidgetSetting("post-avatar", "size", 60);
+    }
+    
+    if (!this.site.mobileView) {
+      this.avatarSize();
+    }
   }
 });
